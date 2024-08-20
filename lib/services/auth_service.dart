2817b5981e671b6e42c4ef17a5f4246e381d4c86
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-20 15:34:48 +0545

    MCQ app

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../studentscreen/student_homepage.dart';
+
+Future<void> signInAndVerify(
+    String email, String password, BuildContext context) async {
+  try {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    // Check if the user exists in Firestore
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userCredential.user?.uid)
+        .get();
+
+    if (!userDoc.exists) {
+      // User does not exist in Firestore, sign out the user
+      await FirebaseAuth.instance.signOut();
+      throw FirebaseAuthException(
+        code: 'user-not-found',
+        message: 'User account does not exist in Firestore.',
+      );
+    }
+
+    // Proceed with navigation if user exists
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (BuildContext context) => StudentHomepage()));
+  } on FirebaseAuthException catch (e) {
+    // Handle authentication exceptions
+    print('Firebase Auth Error: ${e.message}');
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message ?? 'An error occurred')));
+  } catch (e) {
+    // Handle other exceptions
+    print('Error: $e');
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('An unexpected error occurred')));
+  }
+}
