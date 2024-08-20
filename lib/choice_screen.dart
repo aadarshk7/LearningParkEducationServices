b819d82b770e6fdd7ccf7b2f7cd36dea9e7c8b39
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-20 14:17:20 +0545

    MCQ app

--- lib/choice_screen.dart
+++ lib/choice_screen.dart
@@ -1,98 +1,77 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:learningparkeducation/adminpage/adminoptionpage.dart';
 import 'package:learningparkeducation/screens/login_screen.dart';
-import 'package:learningparkeducation/studentscreen/student_login.dart';
-import 'package:learningparkeducation/teacherscreen/teacherhomepage.dart';
-import 'package:learningparkeducation/teacherscreen2/subjectpage.dart';
+import 'package:learningparkeducation/studentscreen/student_homepage.dart';
 import 'package:learningparkeducation/teacherscreen2/teacherhomepage.dart';
-import '../studentscreen/studentssubject_list.dart';
-import '../phoneotp.dart';
-import 'package:learningparkeducation/screens/splash_screen.dart';
-import '../screens/teacher_login_screen.dart';
-import 'studentscreen/student_homepage.dart';
 
-class ChoicePage extends StatefulWidget {
-  @override
-  State<ChoicePage> createState() => _ChoicePageState();
-}
-
-class _ChoicePageState extends State<ChoicePage> with TickerProviderStateMixin {
-  get screenHeight => MediaQuery.of(context).size.height;
-  get screenWidth => MediaQuery.of(context).size.width;
+class ChoicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            // padding: const EdgeInsets.symmetric(horizontal: 47.0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  // padding: const EdgeInsets.symmetric(horizontal: 47.0),
-                  padding: EdgeInsets.fromLTRB(
-                      0, screenHeight * 0.16, screenHeight * 0.01, 0),
-                  child: Container(
-                    height: screenHeight * 0.29,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                            'assets/images/singlelogocompressed.jpg'),
-                        fit: BoxFit.contain,
-                      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    0, screenHeight * 0.16, screenHeight * 0.01, 0),
+                child: Container(
+                  height: screenHeight * 0.29,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/singlelogocompressed.jpg'),
+                      fit: BoxFit.contain,
                     ),
                   ),
                 ),
-                SizedBox(height: screenHeight * 0.02),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 33.0),
-                  child: ShaderMask(
-                    shaderCallback: (bounds) => const LinearGradient(
-                      colors: [Colors.blue, Colors.green],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ).createShader(bounds),
-                    child: const Text(
-                      "LearningPark Education MCQ App",
-                      style: TextStyle(
-                        fontSize: 33,
-                        fontWeight: FontWeight.bold,
-                        color: Colors
-                            .white, // This color will be overridden by the gradient
-                      ),
-                      textAlign: TextAlign.center,
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 33.0),
+                child: ShaderMask(
+                  shaderCallback: (bounds) => const LinearGradient(
+                    colors: [Colors.blue, Colors.green],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ).createShader(bounds),
+                  child: const Text(
+                    "LearningPark Education MCQ App",
+                    style: TextStyle(
+                      fontSize: 33,
+                      fontWeight: FontWeight.bold,
+                      color: Colors
+                          .white, // This color will be overridden by the gradient
                     ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                Container(child: SizedBox(height: screenHeight * 0.09)),
-                _buildChoiceButton(
-                  context: context,
-                  label: "Teacher",
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) => TeacherHomePage(),
-                    ));
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                _buildChoiceButton(
-                  context: context,
-                  label: "Student",
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) => StudentHomepage(),
-                    ));
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.03),
-              ],
-            ),
+              ),
+              SizedBox(height: screenHeight * 0.09),
+              _buildChoiceButton(
+                context: context,
+                label: "Teacher",
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => TeacherHomePage(),
+                  ));
+                },
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              _buildChoiceButton(
+                context: context,
+                label: "Student",
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ));
+                },
+              ),
+              SizedBox(height: screenHeight * 0.03),
+            ],
           ),
         ),
       ),
@@ -105,10 +84,7 @@ class _ChoicePageState extends State<ChoicePage> with TickerProviderStateMixin {
     required VoidCallback onPressed,
   }) {
     return Padding(
-      padding:
-          // EdgeInsets.symmetric(vertical: screenWidth * 0.0),
-          // EdgeInsets.symmetric(vertical: screenHeight * 0.01),
-          const EdgeInsets.symmetric(horizontal: 47.0),
+      padding: const EdgeInsets.symmetric(horizontal: 47.0),
       child: Container(
         height: 50,
         width: double.infinity,
