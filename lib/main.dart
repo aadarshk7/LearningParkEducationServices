cb5ffda7e56a58fe2e38630f1ab7f6538d609471
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-21 21:07:40 +0545

    MCQ app

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import "package:flutter/src/painting/text_style.dart" as ts;
 import 'package:flutter_firebase_recaptcha/firebase_recaptcha_verifier_modal.dart';
 import 'package:learningparkeducation/screens/splash_screen.dart';
+import 'package:provider/provider.dart';
+import '../chatbot/shared_preferences.dart' as sp;
+import '../chatbot/chat_screen.dart';
+import '../chatbot/chat_provider.dart';
 
-// Code to connect with firebase databases
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
-            authDomain: "learningparkeducation.firebaseapp.com",
-            projectId: "learningparkeducation",
-            storageBucket: "learningparkeducation.appspot.com",
-            messagingSenderId: "939288879280",
-            appId: "1:45063901374:web:45063901374",
-            measurementId: "G-FREWCBF7Y1"));
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
+        authDomain: "learningparkeducation.firebaseapp.com",
+        projectId: "learningparkeducation",
+        storageBucket: "learningparkeducation.appspot.com",
+        messagingSenderId: "939288879280",
+        appId: "1:45063901374:web:45063901374",
+        measurementId: "G-FREWCBF7Y1",
+      ),
+    );
   }
-  ;
-
-// await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   const firebaseConfig = {
-//     'apiKey': "AIzaSyDx24fU-veTw_QttLg5mzgAJiDrYXv1EOM",
-//     'authDomain': "loginapp-87017.firebaseapp.com",
-//     'projectId': "loginapp-87017",
-//     'storageBucket': "loginapp-87017.appspot.com",
-//     'messagingSenderId': "939288879280",
-//     'appId': "1:939288879280:web:939288879280",
-//   };
 
   const firebaseConfig = {
     'apiKey': "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
@@ -43,14 +36,12 @@ void main() async {
   await Firebase.initializeApp(
     options: const FirebaseOptions(
       apiKey: "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
-//   api key here
       appId: "learningparkeducation",
-//  app id here
       messagingSenderId: "45063901374",
-// messagingSenderId here
-      projectId: "learningparkeducation", // project id here
+      projectId: "learningparkeducation",
     ),
   );
+
   FirebaseRecaptchaVerifierModal(
     firebaseConfig: firebaseConfig,
     onVerify: (token) => print('token: ' + token),
@@ -59,10 +50,18 @@ void main() async {
     onFullChallenge: () => print('onFullChallenge'),
     attemptInvisibleVerification: true,
   );
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
-  ));
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -86,47 +85,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// // Update the parameter name in the MaterialApp widget
-// return MaterialApp(
-//   debugShowCheckedModeBanner: false,
-//   home: StreamBuilder<User?>(
-//     stream: FirebaseAuth.instance.authStateChanges(),
-//     builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-//       if (snapshot.hasError)
-//       {
-//         return Text(snapshot.error.toString());
-//       }
-//       if (snapshot.connectionState == ConnectionState.active) {
-//         if (snapshot.data == null) {
-//           return Googlescreen();
-//         } else {
-//           return LoginPage(
-//               userName: FirebaseAuth.instance.currentUser!
-//                   .displayName!); // Corrected parameter name
-//         }
-//       }
-//       return Center(child: CircularProgressIndicator());
-//     },
-//   ),
-// );
-
-// class AuthWrapper extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context){
-//     final authService = Provider.of<AuthService>(context);
-//     return StreamBuilder<User?>(
-//       stream: authService.user,
-//       builder: (_, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.active) {
-//           final User? user = snapshot.data;
-//           return user == null ? LoginPage() : HomePage();
-//         } else {
-//           return Scaffold(
-//             body: Center(child: CircularProgressIndicator()),
-//           );
-//         }
-//       },
-//     );
-//   }
-// }
