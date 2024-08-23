63cdadfc391c19e4f92d8ae07214f4bf2f825aac
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-23 21:02:19 +0545

    MCQ app

--- lib/teacherscreen/settings_page.dart
+++ lib/teacherscreen/settings_page.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/material.dart';
-
-class SettingsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('st'),
-      ),
-      body: Center(
-        child: Text('Subjects Page Content'),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class SettingsPage extends StatelessWidget {
+//   const SettingsPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('st'),
+//       ),
+//       body: const Center(
+//         child: Text('Subjects Page Content'),
+//       ),
+//     );
+//   }
+// }
