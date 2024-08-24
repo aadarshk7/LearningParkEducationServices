68bc9c4c11d047f6cf5778f4a1edbcd1de363541
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-24 22:06:54 +0545

    MCQ app

--- lib/teacherscreen/resultpage.dart
+++ lib/teacherscreen/resultpage.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Results', style: TextStyle(fontSize: 24)),
+        title: const Text('Results', style: TextStyle(fontSize: 24)),
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) return CircularProgressIndicator();
+          if (!snapshot.hasData) return const CircularProgressIndicator();
           var users = snapshot.data!.docs;
 
           return ListView.builder(
             itemCount: users.length,
             itemBuilder: (context, index) {
               return ListTile(
-                leading: CircleAvatar(child: Icon(Icons.person)),
+                leading: const CircleAvatar(child: Icon(Icons.person)),
                 title:
-                    Text(users[index]['name'], style: TextStyle(fontSize: 18)),
+                    Text(users[index]['name'], style: const TextStyle(fontSize: 18)),
                 trailing: Text('Marks: ${users[index]['marks']}',
-                    style: TextStyle(fontSize: 18)),
+                    style: const TextStyle(fontSize: 18)),
               );
             },
           );
