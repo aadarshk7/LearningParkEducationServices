2f04b6f83d6550e4f54711f7da63bcab67fdb334
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-25 16:14:57 +0545

    MCQ app

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,58 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_firebase_recaptcha/firebase_recaptcha_verifier_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:learningparkeducation/Prov_screen/pov_teacherprovider.dart';
 import 'package:learningparkeducation/screens/splash_screen.dart';
+import 'package:learningparkeducation/studentscreen/student_homepage.dart';
+import 'package:learningparkeducation/teacherscreen2/teacherhomepage.dart';
 import 'package:provider/provider.dart';
-import '../chatbot/shared_preferences.dart' as sp;
-import '../chatbot/chat_screen.dart';
-import '../chatbot/chat_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'chatbot/chat_provider.dart';
+import 'Prov_screen/prov_authprovider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
-        authDomain: "learningparkeducation.firebaseapp.com",
-        projectId: "learningparkeducation",
-        storageBucket: "learningparkeducation.appspot.com",
-        messagingSenderId: "939288879280",
-        appId: "1:45063901374:web:45063901374",
-        measurementId: "G-FREWCBF7Y1",
-      ),
-    );
-  }
-
-  const firebaseConfig = {
-    'apiKey': "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
-    'authDomain': "learningparkeducation.firebaseapp.com",
-    'projectId': "learningparkeducation",
-    'storageBucket': "learningparkeducation.appspot.com",
-    'messagingSenderId': "939288879280",
-    'appId': "1:45063901374:web:45063901374",
-  };
 
   await Firebase.initializeApp(
     options: const FirebaseOptions(
       apiKey: "AIzaSyCqDgbG5Nau5-8QhmxshXGcZ2R8HQZ-C00",
-      appId: "learningparkeducation",
-      messagingSenderId: "45063901374",
+      authDomain: "learningparkeducation.firebaseapp.com",
       projectId: "learningparkeducation",
+      storageBucket: "learningparkeducation.appspot.com",
+      messagingSenderId: "939288879280",
+      appId: "1:45063901374:web:45063901374",
+      measurementId: "G-FREWCBF7Y1",
     ),
   );
 
-  FirebaseRecaptchaVerifierModal(
-    firebaseConfig: firebaseConfig,
-    onVerify: (token) => print('token: ' + token),
-    onLoad: () => print('onLoad'),
-    onError: () => print('onError'),
-    onFullChallenge: () => print('onFullChallenge'),
-    attemptInvisibleVerification: true,
-  );
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  String userType =
+      prefs.getString('userType') ?? 'users'; // 'student' or 'teacher'
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ChatProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
-      ),
-    ),
-  );
+  runApp(MyApp(isLoggedIn: isLoggedIn, userType: userType));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final bool isLoggedIn;
+  final String userType;
+
+  MyApp({required this.isLoggedIn, required this.userType});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.dark_mode),
-            onPressed: () {
-              // Implement theme switching logic
-            },
-          ),
-        ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+        ChangeNotifierProvider(create: (_) => TeacherAuthProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: isLoggedIn
+            ? (userType == 'teachers' ? TeacherHomePage() : StudentHomepage())
+            : SplashScreen(),
       ),
-      body: const Text("Firebase successfully connected"),
     );
   }
 }
