ea5b4915589f6e301ad70bc037da46df9f35bf60
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-25 16:16:02 +0545

    MCQ app

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:learningparkeducation/choice_screen.dart';
+import 'package:learningparkeducation/studentscreen/student_homepage.dart';
+import 'package:learningparkeducation/teacherscreen2/teacherhomepage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 
-import '../choice_screen.dart';
-
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -12,61 +16,57 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
-      // Navigate to the next screen after the delay.
-      // Replace 'NextScreen()' with your target screen.
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+    String userType = prefs.getString('userType') ?? '';
+
+    // Delay for the splash screen display
+    await Future.delayed(const Duration(seconds: 3));
+
+    // Navigate based on the login status and user type
+    if (isLoggedIn) {
+      if (userType == 'users') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const StudentHomepage()),
+        );
+      } else if (userType == 'teachers') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const TeacherHomePage()),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const ChoicePage()),
+        );
+      }
+    } else {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => ChoicePage()),
+        MaterialPageRoute(builder: (_) => const ChoicePage()),
       );
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/images/logo.jpg'), // Make sure to add your image to the assets folder.
-                fit: BoxFit.contain,
-              ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/images/logo.jpg'),
+              fit: BoxFit.contain,
             ),
           ),
         ),
       ),
-      // backgroundColor: Colors.white,
-      // body: Center(
-      //   child: Container(
-      //     width: 300,
-      //     height: 300,
-      //     decoration: const BoxDecoration(
-      //       shape: BoxShape.circle,
-      //       image: DecorationImage(
-      //         image: AssetImage(
-      //             'assets/images/logo.jpg'), // Make sure to add your image to the assets folder.
-      //         fit: BoxFit.contain,
-      //       ),
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
-
-class NextScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Next Screen')),
-      body: Center(child: Text('Welcome to the Next Screen!')),
-    );
-  }
-}
+// Compare this snippet from lib/Prov_screen/pov_teacherprovider.dart:
