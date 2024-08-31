af27ccc8c9c5aefad7f45ce165bf077c469290be
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-31 16:24:32 +0545

    MCQ app

--- lib/adminpage/student_list.dart
+++ lib/adminpage/student_list.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'admininsertuser.dart';
+import 'student_insert.dart';
 
 class GetUser extends StatefulWidget {
   const GetUser({super.key});
